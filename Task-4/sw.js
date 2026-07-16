632a408d1cf809d7f9edd2307bf4437b8493fe35
Aradhya uttam <aradhyauttam15@gmail.com>
2026-07-16 20:10:25 +0530

    Added sw.js

---
+++ Task-4/sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = "mywebsite-cache-v1";
+const urlsToCache = [
+    "index.html",
+    "css/style.css",
+    "js/script.js"
+];
+
+// Install: pehli baar service worker register hote waqt files cache karo
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+    );
+});
+
+// Fetch: koi bhi request aaye, pehle cache check karo, warna network se lao
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => response || fetch(event.request))
+    );
+});
